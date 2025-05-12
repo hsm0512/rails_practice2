817a86e49a55a83ba16717008a17fe908d48d483
hsm0512 <202653743+hsm0512@users.noreply.github.com>
2025-05-12 18:47:30 +0900

    [追加]画像アップロード機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_12_084429) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_12_092857) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
